8411d0b0f8e7cda61b30d27f62cff5cddc03abfa
Bart Veldstra <bart.veldstra@gmail.com>
2013-04-27 09:46:33 +0200

    Added collider mask to level

--- src/entities/Level.hx
+++ src/entities/Level.hx
@@ -2,6 +2,7 @@ package entities;
  
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Tilemap;
+import com.haxepunk.masks.Grid;
  
 class Level extends Entity
 {
@@ -10,11 +11,14 @@ class Level extends Entity
         super(0, 0);
         background = new Tilemap("gfx/tiles.png", 640, 480, 16, 16);        
         addGraphic(background);
+        colliderMask = new Grid(background.width, background.height, background.tileWidth, background.tileHeight);
+        mask = colliderMask;
         setupLevel();
     }
 
     function setupLevel()
     {
+        // Draw tiles
         background.clearRect(0,0,background.columns, background.rows);
         background.setRect(0,0,background.columns, background.rows, 2);
         background.setRect(1,1,background.columns - 2, background.rows - 2, 1);
@@ -27,6 +31,17 @@ class Level extends Entity
         var corridorWidth:Int = 4;
         var centerIsle:Int = Math.floor((background.columns - corridorWidth) / 2);
         background.setRect(centerIsle,1, corridorWidth, background.rows - 2, 1);
+
+        // Setup mask
+        colliderMask.clearRect(0,0, background.columns, background.rows);
+        for (col in 0...background.columns)
+        {
+            for (row in 0...background.rows)
+            {
+                var solid = background.getTile(col, row) == 2;
+                colliderMask.setTile(col, row, solid);
+            }
+        }
     }
 
     public override function update()
@@ -35,4 +50,5 @@ class Level extends Entity
     }
 
     var background:Tilemap;
+    var colliderMask:Grid;
 }
