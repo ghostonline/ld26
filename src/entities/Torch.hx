4501a92e141f8a8f185371be332be3ca5f8ea1ee
Bart Veldstra <bart.veldstra@gmail.com>
2013-04-28 14:26:57 +0200

    Added particle emitter to the torch

--- src/entities/Torch.hx
+++ src/entities/Torch.hx
@@ -3,9 +3,15 @@ package entities;
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Image;
 import entities.LightMap;
+import com.haxepunk.graphics.Emitter;
+import flash.display.BitmapData;
+import com.haxepunk.utils.Ease;
  
 class Torch extends Entity
 {
+    static var emitterTimeout = 15;
+    static var ParticleName = "embers";
+
     public function new(x:Float, y:Float, source:LightPoint)
     {
         super(x, y);
@@ -14,7 +20,32 @@ class Torch extends Entity
         addGraphic(img);
         this.source = source;
         layer = 60;
+        emitter = new Emitter(new BitmapData(2,2, false, 0xFFFFFFFF), 2, 2);
+        emitter.relative = false;
+
+        emitter.newType(ParticleName, [0]);
+        emitter.setColor(ParticleName, 0xFFFF00, 0xFF8800);
+        emitter.setAlpha(ParticleName, 1, 0);
+        emitter.setMotion(ParticleName, 80, 10, 2, 20, 0, 0, Ease.quadInOut);
+        addGraphic(emitter);
+        emitterTimer = 0;
+    }
+
+    public override function update()
+    {
+        super.update();
+        if (emitterTimer < 0)
+        {
+            emitter.emit(ParticleName, x, y-5);
+            emitterTimer = emitterTimeout;
+        }
+        else
+        {
+            emitterTimer--;
+        }
     }
 
     var source:LightPoint;
+    var emitter:Emitter;
+    var emitterTimer:Int;
 }
